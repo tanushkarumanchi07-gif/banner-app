fe5fba7202f07aa948345c806bbc8314dfd77b80
Tanush Karumanchi <tanushkarumanchi@Tanushs-MacBook-Air-269.local>
2026-02-16 14:18:21 +0530

    UC1: Print OOPS to console

---
+++ OOPSBannerApp.java
@@ -0,0 +1,14 @@
+/**
+ * OOPSBannerApp
+ *
+ * UC1: Prints the word "OOPS" to the console.
+ *
+ * @author Hemanth
+ * @version 1.0
+ */
+public class OOPSBannerApp {
+
+    public static void main(String[] args) {
+        System.out.println("OOPS");
+    }
+}
